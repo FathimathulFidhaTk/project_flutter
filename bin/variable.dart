96d226772f8d454f4ef23e0bcb55dff595f2a22b
fathi <fathifidha2806@gmail.com>
2023-05-22 15:55:31 +0530

    commit

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,16 +1,29 @@
 //class classname{}
-class myclass {
-  String name = "Fidha";
-  int age = 20;
-  double mark = 40.9;
-  static String course = "fluter";
 
+class myclass {
+  late String name;
+  late int age;
+  double ?mark;
+  static String course = "fluter";
 }
-void main(){
-  myclass obj = myclass();// classname objectname=construct();
-  print("My Name is ${obj.name}");
-  print("My Age is ${obj.age}");
-  print("My Mark is ${obj.mark}");
+
+  void main(){
+  myclass obj = myclass(); // classname objectname=construct();
+  print("FIRST STUDENT DETAILS");
+  print("My Name is ${obj.name="Fidha"}");
+  print("My Age is ${obj.age=20}");
+  print("My Mark is ${obj.mark=30.4}");
   print("I had taken course ${myclass.course}");
+  print("");// for space b/w two para
+
+  myclass obj1 = myclass();
+  print("SECOND STUDENT DETAILS");
+  print("My Name is ${obj1.name="Anjana"}");
+  print("My Age is ${obj1.age=21}");
+  print("My Mark is ${obj1.mark=20.3}");
+  print("I had taken course ${myclass.course}");
+
+
+
 
 }
