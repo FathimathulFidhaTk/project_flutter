d20c1e35c56cf37b0f500f59096a00a4f5297953
fathi <fathifidha2806@gmail.com>
2023-05-22 15:07:19 +0530

    commit

---
+++ bin/sample2.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name = "fidha";
+  int age = 20;
+  String course = "fluter";
+  double mark = 30.4;
+
+  print("My Name is $name");
+  print("My Age is $age");
+  print(course);
+  print(mark);
+
+}
