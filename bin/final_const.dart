2129c888b3977d3aadf8118f54ae74c88b642fc8
fathi <fathifidha2806@gmail.com>
2023-05-23 15:56:39 +0530

    commit

---
+++ bin/final_const.dart
@@ -0,0 +1,9 @@
+void main(){
+  String object = "car";
+  int number = 3456;
+  final String colour;
+  colour = "Green";
+  const int num = 8908;
+  print(colour);
+  print(num);
+}
---
+++ bin/var_dynamic.dart
@@ -0,0 +1,7 @@
+void main(){
+  var name = "Fidha";
+  dynamic age = 20; //type kodukne anusrch data chnge avu
+  print(name);
+  print(age);
+
+}
