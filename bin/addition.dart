86808f1d9587418fa6cc4b08d0f31968f9662448
fathi <fathifidha2806@gmail.com>
2023-05-23 15:02:15 +0530

    commit

---
+++ bin/addition.dart
@@ -0,0 +1,35 @@
+void main(){
+  add();
+  sub();
+  mul();
+  div();
+}
+
+void add(){
+  int a = 10,
+  b = 20,
+  sum = a+b;
+  print("$a + $b = $sum");
+}
+
+void sub(){
+  int a = 50,
+  b= 20,
+  sub = a-b;
+  print("$a - $b = $sub");
+}
+
+void mul(){
+  int a = 2,
+  b = 5,
+  mul = a*b;
+  print("$a * $b = $mul");
+}
+
+void div(){
+  int a = 10,
+  b = 2,
+  div = a~/b; //integer o/p kitan
+ // double div = a/b; // point value kitan
+  print("$a / $b = $div");
+}
---
+++ bin/function.dart
@@ -0,0 +1,9 @@
+void main() {
+  show();
+}
+void show(){
+  String name = "Fidha";
+  int age = 20;
+  print("My Name is $name");
+  print("My Age is $age");
+}
