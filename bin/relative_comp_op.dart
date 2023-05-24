4c222424e2e1c535b1c89fce42c727d4980c952f
fathi <fathifidha2806@gmail.com>
2023-05-24 15:54:06 +0530

    commit

---
+++ bin/relative_comp_op.dart
@@ -0,0 +1,16 @@
+void main(){
+  int age = 20;
+  print(age > 20);
+  print(age < 20);
+  print(age >= 20);
+  print(age <= 20);
+  print(age == 20);
+  print(age != 20);
+  print("");
+  print(age == 10);
+  print(age > 10);
+  print(age < 10);
+  print(age >= 10);
+  print(age <= 10);
+  print(age != 10);
+}
