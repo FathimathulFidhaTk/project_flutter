3ba79b965a75bf55dbcb30e2e2a2b91aeb426b80
fathi <fathifidha2806@gmail.com>
2023-05-24 14:49:57 +0530

    commit

---
+++ bin/arithmetic_op.dart
@@ -0,0 +1,11 @@
+void main(){
+  var a = 20,
+  b = 10;
+  print("a + b = ${a+b}");
+  print("a - b = ${a-b}");
+  print("a * b = ${a*b}");
+  print("a / b = ${a/b}");
+  print("a ~/ b = ${a~/b}");
+  print("a % b = ${a%b}");
+  print("-(a+b) = ${-(a+b)}");
+}
